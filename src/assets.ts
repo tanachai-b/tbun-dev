49b11b3f221729bd6d64d79cd87953bac097175d
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-08-18 15:40:10 +0700

    developers: organize public assets folder

--- src/assets.ts
+++ src/assets.ts
@@ -1,5 +1,5 @@
-export { default as color_swatches } from "/assets/color-swatches.svg";
-export { default as email_logo } from "/assets/email-logo.svg";
-export { default as github_logo } from "/assets/github-logo.svg";
-export { default as linkedin_logo } from "/assets/linkedin-logo.svg";
-export { default as sticky_notes } from "/assets/sticky-notes.svg";
+export { default as color_swatches } from "/assets/apps-color-swatches.svg";
+export { default as sticky_notes } from "/assets/apps-sticky-notes.svg";
+export { default as email_logo } from "/assets/contacts-email.svg";
+export { default as github_logo } from "/assets/contacts-github.svg";
+export { default as linkedin_logo } from "/assets/contacts-linkedin.svg";
