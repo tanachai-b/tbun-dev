3125feb269fc3712301851b2c2f8a56e0b32354b
Tanachai Bunlutangtum <tanachai.bun@gmail.com>
2024-08-20 23:03:11 +0700

    new feature: update url path when navigate

--- src/hooks/useNavigation.tsx
+++ src/hooks/useNavigation.tsx
@@ -1,9 +1,12 @@
 import { RefObject, useEffect, useState } from "react";
 
-export function useNavigation() {
-  type Page = "apps" | "about" | "resume";
+const pages = ["apps", "about", "resume"] as const;
+type Page = (typeof pages)[number];
 
-  const [page, setPage] = useState<Page>("apps");
+export function useNavigation() {
+  const initialPage = getPageFromPath();
+
+  const [page, setPage] = useState<Page>(initialPage);
   const [isPageVisible, setIsPageVisible] = useState(false);
 
   useEffect(() => {
@@ -11,6 +14,8 @@ export function useNavigation() {
   }, []);
 
   function navigateTo(toPage: Page, ref?: RefObject<HTMLDivElement>) {
+    updatePath(toPage === "apps" ? "" : toPage);
+
     if (toPage === page) {
       if (ref != null) {
         ref.current?.scrollIntoView({ behavior: "smooth" });
@@ -37,4 +42,23 @@ export function useNavigation() {
   }
 
   return { page, isPageVisible, navigateTo };
+}
+
+function getPageFromPath() {
+  const baseUrl = import.meta.env.BASE_URL;
+  const fullPath = location.pathname;
+
+  const path = fullPath.substring(baseUrl.length);
+  const page = isPage(path) ? path : "apps";
+
+  return page;
+}
+
+function isPage(path: string): path is Page {
+  return pages.includes(path as Page);
+}
+
+function updatePath(path: string) {
+  const baseUrl = import.meta.env.BASE_URL;
+  history.pushState({}, "", `${baseUrl}${path}`);
 }
